2ee027a980869b1b0b381dd362f9e51241d21b0b
Carloto <viniciuscarloto@gmail.com>
2020-08-19 18:45:49 -0300

    added rgb to cmyk conversion

--- color-converter/color-converter.py
+++ color-converter/color-converter.py
@@ -4,13 +4,33 @@ import sys
 
 def removeChannels(img, a, b):
     '''Remove two channels from the image.'''
-    height, width, depth = img.shape
-    img[0:height, 0:width, a] = 0
-    img[0:height, 0:width, b] = 0
+    img[:,:,a] = 0
+    img[:,:,b] = 0
 
-img = cv.imread(sys.argv[1]) # Image in BGR color format
-blue = img.copy()
-removeChannels(blue, 1, 2)
-cv.imshow('blue',blue)
-cv.waitKey(0)
-cv.destroyAllWindows()
+
+def main():
+    img = cv.imread(sys.argv[1]) # Image in BGR color format
+
+    blue = img[...,0]
+    green = img[...,1]
+    red = img[...,2]
+
+    # RGB para CMYK
+    
+    cyan = 1 - (red/255)
+    magenta = 1 - (green/255)
+    yellow = 1 - (blue/255)
+
+    black = np.amin((cyan, magenta, yellow), axis=0)
+
+    cyan = (cyan - black)/(1 - black)
+    magenta = (magenta - black)/(1 - black)
+    yellow = (yellow - black)/(1 - black)
+
+    cv.imwrite('cmyk.png',cv.merge((cyan*255,magenta*255,yellow*255)))
+
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
