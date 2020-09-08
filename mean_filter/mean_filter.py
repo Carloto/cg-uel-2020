d0e9aca62c75d1ab42e5573788adba9d06bb0305
Carloto <viniciuscarloto@gmail.com>
2020-09-07 22:55:51 -0300

    implemented weighted mean filter

--- mean_filter/mean_filter.py
+++ mean_filter/mean_filter.py
@@ -3,14 +3,14 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-def mean_filter(img):
+def mean_filter(img, m):
     """Aplica o filtro da média"""
     mean_img = img.copy()
     for row in range(img.shape[0] - 2):
         for column in range(img.shape[1] - 2):
             for channel in range(3):
                 mean_img[row+1, column+1,
-                         channel] = mean(mean_img, row+1, column+1, channel)
+                         channel] = m(mean_img, row+1, column+1, channel)
     return mean_img
 
 
@@ -24,11 +24,29 @@ def mean(img, row, column, channel):
     return np.array(sum(mean)/9)
 
 
+def weighted_mean(img, row, column, channel):
+    """Calcula a média ponderada da região 3x3 de dado pixel"""
+    mean = []
+    mean.append(img[row, column, channel]*4)
+    mean.append(img[row, column+1, channel]*2)
+    mean.append(img[row, column-1, channel]*2)
+    mean.append(img[row-1, column, channel]*2)
+    mean.append(img[row+1, column, channel]*2)
+    mean.append(img[row-1, column-1, channel])
+    mean.append(img[row-1, column+1, channel])
+    mean.append(img[row+1, column-1, channel])
+    mean.append(img[row+1, column+1, channel])
+
+    return np.array(sum(mean)/16)
+
+
 def main():
     img = cv.imread('../tokyo.jpg')
 
-    mean_img = mean_filter(img)
+    mean_img = mean_filter(img, mean)
+    w_mean_img = mean_filter(img, weighted_mean)
     cv.imwrite("mean_filter.jpg", mean_img)
+    cv.imwrite("weighted_mean_filter.jpg", w_mean_img)
 
 
 if __name__ == '__main__':
